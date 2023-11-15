72d09b38f24d328af546de1e615f8c1c4e5b57e8
ltblueberry <aromanov.app@gmail.com>
2023-11-15 13:42:05 +0100

    ec2 instance profile deployment added

---
+++ ec2-instance-profile.tf
@@ -0,0 +1,23 @@
+# IAM Role to be used in IAM Instance Profile
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+# IAM Instance Profile for EC2 instances
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
