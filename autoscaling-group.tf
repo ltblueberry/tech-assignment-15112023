57edd5c5ef5e9c414fcd0d2d93dbee99f30cb176
ltblueberry <aromanov.app@gmail.com>
2023-11-15 17:09:11 +0100

    finalizing autoscaling group

--- autoscaling-group.tf
+++ autoscaling-group.tf
@@ -17,33 +17,29 @@ module "autoscaling" {
 
   name = "app-autoscaling-group"
 
-  vpc_zone_identifier = module.vpc.public_subnets
-  min_size            = 0
-  max_size            = 1
-  desired_capacity    = 1
+  vpc_zone_identifier = module.vpc.private_subnets
+  min_size            = 1
+  max_size            = 5
+  desired_capacity    = 3
 
   image_id      = "ami-0a485299eeb98b979" # Amazon Linux ami-0a485299eeb98b979 (64-bit (x86)) / ami-0ca82fa36091d6ada (64-bit (Arm))
   instance_type = "t2.micro"
 
   target_group_arns = [module.alb.target_groups.instance.arn]
+  user_data = base64encode(local.user_data)
 
   network_interfaces = [
     {
       delete_on_termination       = true
-      associate_public_ip_address = true
+      associate_public_ip_address = false
       description                 = "eth0"
       device_index                = 0
       security_groups             = [module.sg.security_group_id]
     }
   ]
 
-  user_data = base64encode(local.user_data)
-
   tags = {
     Terraform   = "true"
     Environment = "develop"
   }
-
-  # FOR DEBUG
-  key_name = "debug-rsa"
 }
